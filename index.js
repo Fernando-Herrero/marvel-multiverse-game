2a3100bc7b8dcd116913c3425c69ed5db619e1b8
Fernando Herrero <herrerofernando.94@gmail.com>
2025-04-25 18:07:42 +0200

    add map img and html

--- index.js
+++
@@ -1,9 +0,0 @@
-const options = {
-	method: "GET",
-	headers: { accept: "application/json", "x-cg-demo-api-key": "CG-Fxd3bgMWh5hcViBE52R6yhqY" },
-};
-
-fetch("https://api.coingecko.com/api/v3/coins/list", options)
-	.then((res) => res.json())
-	.then((res) => console.log(res))
-	.catch((err) => console.error(err));
---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+import { fetchMarvelData } from "./utils";
+
+// const displayCharacters = async () => {
+// 	const characters = await fetchMarvelData("characters");
+// 	if (characters) {
+// 		console.log(characters); 
+// 	} else {
+// 		console.log("Error fetching characters.");
+// 	}
+// };
+
+// displayCharacters();
+
